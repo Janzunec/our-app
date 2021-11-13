c6493a57d768d75e212088fce3fb8198f6b19cbc
Jan Žunec <zunec.jan@gmail.com>
2021-11-13 10:38:41 +0100

    added slider to skills

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from 'react';
 
-export default function Skills() {
-  return (
-    <>
-      <h1>TO JE SKILLS... Work in progress... LULEK</h1>
-    </>
-  );
-}
+const Skills: React.FC = () => {
+	const first = Math.floor(Math.random() * 2) === 1 ? true : false;
+
+	const [isChecked, setIsChecked] = useState(first);
+
+	const handleOnChange = () => {
+		setIsChecked(!isChecked);
+		console.log(isChecked);
+	};
+
+	return (
+		<div className='w-screen h-screen pt-20'>
+			<div className='form-control flex align-middle items-center justify-center flex-row'>
+				{/* <label className='cursor-pointer label'> */}
+				<span className='label-text text-3xl mr-6'>Jan</span>
+				<input
+					type='checkbox'
+					className='toggle toggle-primary toggle-lg bg-opacity-100 border-opacity-100'
+					checked={isChecked}
+					onChange={handleOnChange}
+				></input>
+				<span className='label-text text-3xl ml-6 text-secondary'>
+					Mark
+				</span>
+				{/* </label> */}
+			</div>
+		</div>
+	);
+};
+
+export default Skills;
