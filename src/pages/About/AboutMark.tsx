2b193ad4ea0d8273d125a0b68aae9bb2ac2888f1
Vrechko <vrckomark@gmail.com>
2021-11-16 18:32:25 +0100

    Added Spotify Activity in AboutMark

--- src/pages/About/AboutMark.tsx
+++ src/pages/About/AboutMark.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import Slider from '../../components/Slider';
-import LightUpString from '../../components/LightUpString';
-import { useLanyard } from 'use-lanyard';
-import SpotifyMark from '../../components/SpotifyMark';
+import React from "react";
+import Slider from "../../components/Slider";
+import LightUpString from "../../components/LightUpString";
+import { useLanyard } from "use-lanyard";
+import SpotifyMark from "../../components/SpotifyMark";
 
 interface AboutMarkProps {}
 
 const AboutMark: React.FC<AboutMarkProps> = () => {
-	const { data: activity } = useLanyard('290867016468791297', {
-		refreshInterval: 2000,
-		revalidateOnFocus: false,
-		refreshWhenHidden: true
-	});
+  const { data: activity } = useLanyard("290867016468791297", {
+    refreshInterval: 2000,
+    revalidateOnFocus: false,
+    refreshWhenHidden: true,
+  });
 
-	return (
-		<div className='min-w-screen min-h-screen pt-20'>
-			<Slider site={'about'} />
-			<div className='w-screen flex flex-row '>
-				{activity?.spotify && (
-					<SpotifyMark spotify={activity.spotify} />
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className="min-w-screen min-h-screen pt-20">
+      <Slider site={"about"} />
+      <div className="w-screen flex flex-row text-white ">
+        <SpotifyMark spotify={activity?.spotify ?? undefined} />
+      </div>
+    </div>
+  );
 };
 
 export default AboutMark;
